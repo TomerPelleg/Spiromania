01d909152abc6b47e0d231fbe8fb67f88d9be0a9
Tomer Pelleg <tomer@pelleg.org>
2022-07-08 20:08:48 +0300

    Combine two projects

---
+++ fourier.py
@@ -0,0 +1,98 @@
+import cmath
+from copy import deepcopy
+
+import numpy as np
+import pygame
+from spiro import Spiro
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+PI = cmath.pi
+width, height = (1000, 700)
+start = np.asarray((width // 2, height // 2))
+
+
+def create_function(points):
+	xs = np.linspace(-1*PI, PI, np.shape(points)[0])
+	ys = points[:,0] + points[:,1] * 1j
+	return xs, ys
+
+
+def num(k):
+	return (-1) ** k * (k + 1) // 2
+
+
+def calc_coeffs(xs, ys, k):
+	cfs = np.zeros(k, dtype=complex)
+	for n in range(k):
+		y_cpy = np.array([cmath.exp(-1j * num(n) * xs[ni]) for ni in range(len(xs))])
+		cfs[n] = sum(ys * y_cpy) / (len(xs)-1)
+	return cfs
+
+
+def fourier_main():
+	pygame.init()
+	global screen
+	screen = pygame.display.set_mode((1000, 700))
+
+	screen.fill(WHITE)
+	# pygame.draw.rect(screen, (0,0,0), pygame.Rect(0,0,50,50))
+	pygame.display.update()
+
+	ps = np.full((10000, 2), 0, dtype=float)
+	i = 0
+	k = 50
+	add_to_arr = False
+	r = pygame.Rect(0, 0, 10, 10)
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.display.quit()
+				pygame.quit()
+				return
+
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_RETURN:		# IF ENTER IS PRESSED
+					ps = ps[:i]-start
+					xs, ys = create_function(ps)
+					coeffs = calc_coeffs(xs, ys, k)
+					speeds = np.array([num(ki) for ki in range(k)])
+					s = Spiro(speeds, coeffs, screen)
+
+					rs = np.array(coeffs, dtype=int) ** 2 + np.array(-1j * coeffs, dtype=int) ** 2
+					# s = OldSpiro(, [150,-35], , screen)
+
+					pygame.display.quit()
+					pygame.quit()
+					return
+
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				ps = np.full((1000, 2), 0, dtype=float)
+				i = 0
+				add_to_arr = True
+
+			if event.type == pygame.MOUSEBUTTONUP:
+				if len(ps)<2:
+					add_to_arr = False
+					continue
+				pos = pygame.mouse.get_pos()
+				add_to_arr = False
+				screen.fill(WHITE)
+				pygame.draw.lines(screen, (0, 0, 0), True, ps[:i], width=5)
+				# for p in ps:
+				# r.move_ip(pos)
+				# pygame.surface
+				# pygame.draw.rect(screen, (0,0,0), pygame.Rect(p, (50,50)))
+				pygame.display.update()
+			if add_to_arr:
+				pos = pygame.mouse.get_pos()
+				pygame.draw.rect(screen, (0, 0, 175), pygame.Rect(pos, (10, 10)))
+				print(pos)
+				ps[i] = pos
+				i += 1
+		# pygame.draw.lines(screen, (0,0,0), True, ps[:i], width=5)
+
+
+if __name__ == '__main__':
+	fourier_main()
---
+++ spiro.py
@@ -0,0 +1,85 @@
+import pygame
+import numpy as np
+from math import cos, sin, pi
+from time import sleep
+import cmath
+
+# POSITIVE RADIUS MEANS OUTSIDE
+
+# optimizations:
+# https://stackoverflow.com/questions/70092416/pygame-efficient-way-to-draw-traceline-of-moving-object
+# also, pygame.display.update() can receive a rect to update instead of the entire screen
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREY = (100, 100, 100)
+RED = (255, 0, 0)
+TRK = (0, 180, 120)
+BLUE = (0, 0, 255)
+alpha = 0
+
+width, height = (1000, 700)
+start = np.asarray((width // 2, height // 2))
+
+
+def rep(z):
+	return (z.real, z.imag)
+
+
+class Spiro:
+	def __init__(self, speeds, cfs, screen=None):
+		pygame.init()
+		if screen is None:
+			self.screen = pygame.display.set_mode((width, height))
+		else:
+			self.screen = screen
+
+		self.screen.fill(WHITE)
+
+		trace_l = int(1e5)
+		trace = np.full((trace_l, 2), (0, 0))  # point
+		t, i = 0, 0
+
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.display.quit()
+					pygame.quit()
+					return
+
+			pygame.display.update()
+			self.screen.fill(WHITE)
+
+			# LINES WE USED TO HAVE. WE NEED THEM IF WE WANT TO DRAW CIRCLES
+			# partial_sum = [sum(cfs[:w]) for w in range(len(cfs))]
+			# partial_sum = [rep(jj) for jj in partial_sum]
+			# centers = [partial_sum[2*w] for w in range(len(cfs)//2)]
+			# fixed_points = [partial_sum[2*w+1] for w in range(len(cfs)//2)]
+
+			sum_v = width // 2 + 1.j * (height // 2)
+			rotator = [cmath.exp(1.j * speeds[n] * t) for n in range(len(cfs))]
+			cfs *= rotator
+
+			for q in cfs:
+				pygame.draw.lines(self.screen, BLUE, False, [rep(sum_v),rep(q+sum_v)], width=2)
+				sum_v += q
+			trace[i] = rep(sum_v)
+
+			if i > 2:
+				pygame.draw.lines(self.screen, RED, False, trace[:i], width=2)
+			t += 1e-5
+			i = (i + 1) % trace_l
+
+	# def new_draw_spiro(self, centers, fixed_points, alpha=0.5):
+	# 	dist = (centers - fixed_points) ** 2
+	# 	dist = (dist[:, 0] + dist[:, 1]) ** 0.5
+	# 	radii = dist
+	#
+	# 	centers += start
+	# 	fixed_points += start
+	#
+	# 	for i, r in enumerate(radii):
+	# 		pygame.draw.circle(self.screen, BLACK, centers[i], r, width=3)
+	# 		if i > 0:
+	# 			pygame.draw.line(self.screen, GREY, centers[i], fixed_points[i], width=3)
+	# 	return alpha * centers[-1] + (1 - alpha) * fixed_points[-1]
