6efb9d31cf3f851adb6cef4dfff6e5b6b9f41a1e
Tomer Pelleg <tomer@pelleg.org>
2022-07-08 19:00:23 +0300

    Line search for mario sunshine (works about as good as the game)

--- main.py
+++ main.py
@@ -5,19 +5,30 @@ import time
 from shapely.geometry.polygon import Polygon
 from shapely.geometry import Point
 from inputshape import InputShape
+import math
+import sympy
 
 s0 = [(0, 0), (5, 0), (6, 6), (3, 8), (1, 4), (2, 2)]
 s1 = [(-1,1), (4,-4), (5,-2), (11,1), (10,5), (3,15)]
 
+def create_regular_polygon(x, length):
+	listo = []
+	radius = length / sympy.sin(sympy.pi/x)
+	for i in range(x):
+		listo.append((radius*sympy.cos(2*i*sympy.pi/x) - radius, radius*sympy.sin(2*i*sympy.pi/x)))
+	return listo
+
 square = [(0,0),(3,0),(3,3),(0,3)]
 triangle = [(0,0),(0.5,(3**0.5)/2),(1,0)]
+pentagon = [(0,0), (2,0), (2.62,1.9), (1,3.08), (-0.62,1.9)]
+hexagon = [(0,0), (6,0), (9,5.2), (6, 10.4), (0,10.4), (-3, 5.2)]
 
 
 def main():
-	inside_shape = np.asarray(triangle,
-					dtype=np.float64)*50 + (100,150)
-	outside_shape = np.asarray(square,
-					dtype=np.float64)*50 + (100,150)
+	inside_shape = np.asarray(create_regular_polygon(8, 3),
+					dtype=np.float64)*10 + (300,300)
+	outside_shape = np.asarray(create_regular_polygon(13, 3),
+					dtype=np.float64)*10 + (300,300)
 	inside_Shape = Shape(inside_shape)
 	outside_Shape = Shape(outside_shape)
 	outside_Polygon = Polygon(outside_Shape.cur_points)
@@ -57,11 +68,12 @@ def main():
 		for i in range(5):
 			inside_Shape.calc_rotated(outside_Shape.cur_points, outside_Polygon)
 		screen.fill((255,255,255))
-		inside_Shape.draw_shape(screen, ((2*cntr+200)%255,(9*cntr)%255,(4*cntr+100)%255))
+		# inside_Shape.draw_shape(screen, ((2*cntr+200)%255,(9*cntr)%255,(4*cntr+100)%255))
+		inside_Shape.draw_shape(screen, (0, 0, 255))
 		outside_Shape.draw_shape(screen)
 		button1.prepare()
 		pygame.display.update()
-		clock.tick(20)
+		clock.tick(100)
 		cntr += 1
 
 	# while True:
--- shape.py
+++ shape.py
@@ -1,10 +1,10 @@
+import math
 from math import *
 
 import numpy as np
 import pygame
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
-
 
 class Shape:
 	def __init__(self, these_points):
@@ -15,11 +15,14 @@ class Shape:
 
 		self.special_point = [1 / 3, 1 / 3, 1 / 3]
 		self.trace = []
+		self.contact = False
+		self.factor = 1
 
 	# self.cur_point_position
 	# self.cur_angle = 0
 
 	def calc_rotated(self, big_shape_points: list = None, big_shape_poly: Polygon = None, alpha=0.01):
+		alpha *= self.factor
 		# cur_point = self.cur_points[self.cur_point_index]
 		new_points = [self.cur_points[i] for i in range(len(self.cur_points))]
 		for i in range(len(self.cur_points)):
@@ -32,27 +35,46 @@ class Shape:
 			point += self.axis_point
 			new_points[i] = point
 
-		for p in new_points:
+		our_poly = Polygon(new_points)
+		for idx in range(len(new_points)):
+			p = new_points[idx]
 			point = Point(p)
 			if not big_shape_poly.contains(point) and tuple(self.axis_point) != tuple(p):
-				self.axis_point = p
+				if(self.factor > 1/(2**40)):
+					self.factor/=2
+					return False
+				self.axis_point = self.cur_points[idx]
+				if not self.contact:
+					self.trace = []
+				self.contact = True
+				self.factor = 1
 				return False
 
-		our_poly = Polygon(self.cur_points)
 		for ext_p in big_shape_points:
 			if tuple(self.axis_point) == tuple(ext_p):
 				continue
 			if our_poly.contains(Point(ext_p)):
+				if(self.factor >= 1/(2**40)):
+					self.factor/=2
+					return False
 				self.axis_point = np.array([ext_p[0], ext_p[1]])
+				if not self.contact:
+					self.trace = []
+				self.contact = True
+				self.factor = 1
 				return False
 
 		self.cur_points = new_points
+		mid_point = sum(self.cur_points) / len(self.cur_points)
 		self.trace.append(np.dot(self.special_point, self.cur_points[:len(self.special_point)]))
+		# self.trace.append(mid_point)
 		return True
 
 	def draw_shape(self, screen, color=(255,0,0)):
 		# shape
+		mid_point = np.dot(self.special_point, self.cur_points[:len(self.special_point)])
 		pygame.draw.lines(screen, (0, 0, 0), True, self.cur_points, width=2)
+		pygame.draw.circle(screen, (255, 0,0 ), mid_point, radius=5)
 		# trace
 		if len(self.trace) > 1:
 			pygame.draw.lines(screen, color, False, self.trace, width=2)
