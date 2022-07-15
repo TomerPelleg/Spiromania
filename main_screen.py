615dabef7c59c5f64c8a909db44e937cccc75f18
nissinissi <laelnisi@gmail.com>
2022-07-16 00:27:27 +0300

    :D

--- main_screen.py
+++ main_screen.py
@@ -4,12 +4,13 @@ import fourier
 import old_spiro
 import poly_main
 
-fourier_button = button.TextButton(pos = (0, 10), size =(300, 100), color = (15,15,200), text = "custom drawing", elevation=5)
+fourier_button = button.BoolButton(pos = (0, 10), size =(300, 100), color = (15,15,200), text = "custom drawing", elevation=5)
 old_spiro_button = button.BoolButton(pos = (400, 10), size =(300, 100), color = (15,200,15), text = "symmetric circles", elevation=5)
 polygons_button = button.BoolButton(pos = (800, 10), size =(300, 100), color = (200,15,15), text = "draw polygons", elevation=5)
-
 buttons = [fourier_button, old_spiro_button, polygons_button]
 
+for button in buttons:
+	button.print_text = True
 
 def main_screen_main():
 	pygame.init()
@@ -25,7 +26,10 @@ def main_screen_main():
 
 			if fourier_button.process_clicked(event, screen):
 				r = fourier.fourier_main()
-
+				if r == False:
+					exit()
+				else:
+					screen = pygame.display.set_mode((1100, 700))
 
 			elif old_spiro_button.process_clicked(event, screen):
 				r = old_spiro.old_spiro_main()
@@ -36,8 +40,10 @@ def main_screen_main():
 
 			elif polygons_button.process_clicked(event, screen):
 				r = poly_main.poly_main()
-
-				pygame.display.set_mode((1100, 700))
+				if r == False:
+					exit()
+				else:
+					screen = pygame.display.set_mode((1100, 700))
 
 		pygame.display.update()
 		screen.fill((255,255,255))
